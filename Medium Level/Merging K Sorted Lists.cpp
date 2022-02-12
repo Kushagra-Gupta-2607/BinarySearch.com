4fd34b236a39a82397d281939637b906dcffcda7
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-02-12 22:38:01 +0530

    Create Merging K Sorted Lists.cpp

---
+++ Medium Level/Merging K Sorted Lists.cpp
@@ -0,0 +1,21 @@
+/* Problem -
+https://binarysearch.com/problems/Balanced-Brackets
+
+Q. Given a list of sorted lists of integers, merge them into one large sorted list.
+*/
+
+vector<int> solve(vector<vector<int>>& lists) {
+    map<int, int> mp;
+    int cnt = 0, k = 0;
+    for(int i = 0; i<lists.size(); i++){
+        for(int j = 0; j<lists[i].size(); j++){
+            mp[lists[i][j]]++;
+            cnt++;
+        }
+    }
+    vector<int> ans(cnt);
+    for(auto it: mp){
+        while(it.second) ans[k++] = it.first, it.second--;
+    }
+    return ans;
+}
