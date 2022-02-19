349de5d63570fb29148dac00acd0ea29e16d566e
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-02-19 14:12:33 +0530

    Create Fibonacci Subset Sum.cpp

---
+++ Medium Level/Fibonacci Subset Sum.cpp
@@ -0,0 +1,18 @@
+/* Problem -
+https://binarysearch.com/problems/Fibonacci-Subset-Sum
+
+Q. Given an integer n, return the minimum number of Fibonacci numbers required to sum up to n.
+*/
+
+int solve(int n) {
+    vector<int> v(2, 1);
+    int i = 0, ans = 0;
+    while(v[i]+v[i+1]<=n){
+        v.push_back(v[i]+v[i+1]);
+        i++;
+    }
+    i = v.size()-1;
+    for(; i>-1 && n>0; i--)
+        if(v[i]<=n) n-=v[i], ans++;
+    return ans;
+}
